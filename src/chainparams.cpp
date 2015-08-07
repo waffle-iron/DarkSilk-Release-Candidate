092e3acab2db917d08d26aee4444062dc63a3acd
SCDeveloper <silkcoindeveloper@gmail.com>
2015-08-07 10:42:40 +0100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,10 +121,10 @@ public:
         // The message start string is designed to be unlikely to occur in normal data.
         // The characters are rarely used upper ASCII, not valid as UTF-8, and produce 
         // a large 4-byte int at any alignment.
-        pchMessageStart[0] = 0x24;
-        pchMessageStart[1] = 0x20;
-        pchMessageStart[2] = 0x04;
-        pchMessageStart[3] = 0x42;
+        pchMessageStart[0] = 0x1f;
+        pchMessageStart[1] = 0x22;
+        pchMessageStart[2] = 0x05;
+        pchMessageStart[3] = 0x31;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
         vAlertPubKey = ParseHex("");
         nDefaultPort = 31750;
