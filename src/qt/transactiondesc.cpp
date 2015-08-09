191b69445c122d461a80089c9e7551cfff57f208
SCDeveloper <silkcoindeveloper@gmail.com>
2015-08-09 15:02:53 +0100

    Update transactiondesc.cpp

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -29,7 +29,7 @@ QString TransactionDesc::FormatTxStatus(const CWalletTx& wtx)
         int nDepth = wtx.GetDepthInMainChain();
         if (nDepth < 0)
             return tr("conflicted");
-        else if (GetAdjustedTime() - wtx.nTimeReceived > 4.2 * 60 && wtx.GetRequestCount() == 0)
+        else if (GetAdjustedTime() - wtx.nTimeReceived > 2 * 60 && wtx.GetRequestCount() == 0)
             return tr("%1/offline").arg(nDepth);
         else if (nDepth < 10)
             return tr("%1/unconfirmed").arg(nDepth);
