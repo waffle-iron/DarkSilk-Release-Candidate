9ddfec644333ee7dfca73d9b515b30d50be896ca
SCDeveloper <silkcoindeveloper@gmail.com>
2015-10-04 20:07:43 +0100

    Shadowchat Messaging Updates

--- src/net.h
+++ src/net.h
@@ -208,6 +208,7 @@ public:
     
     ~SecMsgNode() {};
     
+    CCriticalSection            cs_smsg_net;
     int64_t                     lastSeen;
     int64_t                     lastMatched;
     int64_t                     ignoreUntil;
