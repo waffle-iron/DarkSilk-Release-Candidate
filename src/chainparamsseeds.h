1f7055952462f1894deeafc47e0aaa68ca8b9c8f
SCDeveloper <silkcoindeveloper@gmail.com>
2015-10-12 11:54:14 +0100

    Update chainparamsseeds.h

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -3,13 +3,14 @@
 
 static const unsigned int pnSeed[] =
 {
-	0x2d3f0d80,    
+	0x2d3f0d80,
+	0x68ee8a7a,
+   	0x2d3f07e1,
+   	0x2d3f0e44,
 };
 
 static const unsigned int pnTestnetSeed[] =
 {
-   0x2d3f0d80,
 };
-
 
 #endif
