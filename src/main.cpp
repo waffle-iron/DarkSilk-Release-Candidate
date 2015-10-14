8caaf5783664bb59fde5e4934710e378e4d495f1
SCDeveloper <silkcoindeveloper@gmail.com>
2015-10-14 13:41:31 +0100

    static const rename | nActualSpacing code reform

--- src/main.cpp
+++ src/main.cpp
@@ -1190,17 +1190,22 @@ unsigned int GetNextTargetRequired(const CBlockIndex* pindexLast, bool fProofOfS
     int64_t nActualSpacing = pindexPrev->GetBlockTime() - pindexPrevPrev->GetBlockTime();
 
     if (nActualSpacing < 0)
+    {
         nActualSpacing = POW_TARGET_SPACING;
-
-    if (nActualSpacing > POW_TARGET_SPACING * 10)
+    }
+    else if (nActualSpacing > POW_TARGET_SPACING * 10)
+    {
         nActualSpacing = POW_TARGET_SPACING * 10;
-
+    }
+    else 
+        nActualSpacing = POW_TARGET_SPACING;
+    
     // target change every block
     // retarget with exponential moving toward target spacing
     // Includes fix for wrong retargeting difficulty by Mammix2
     CBigNum bnNew;
     bnNew.SetCompact(pindexPrev->nBits);
-    int64_t nInterval = TARGET_TIME_SPAN / POW_TARGET_SPACING; // equals 1 and this means diff is retargeted each block
+    int64_t nInterval = POS_TARGET_SPACING / POW_TARGET_SPACING; // 4 / 4 = 1
     bnNew *= ((nInterval - 1) * POW_TARGET_SPACING + nActualSpacing + nActualSpacing);
     bnNew /= ((nInterval + 1) * POW_TARGET_SPACING);
 
