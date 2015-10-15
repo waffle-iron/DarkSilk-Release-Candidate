c74e7e8749b6581900b656e31c5e5f7762ffc787
SCDeveloper <silkcoindeveloper@gmail.com>
2015-10-15 15:46:04 +0100

    Timing Changes

--- src/main.h
+++ src/main.h
@@ -92,8 +92,8 @@ static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov 5th 00:53:2
 
 static const unsigned int TARGET_TIME_SPAN = 4 * 60;  // 4 mins
 static const unsigned int POW_TARGET_SPACING = 4 * 60; // 4 mins
-static const unsigned int TARGET_SPACING = 64
-static const int64_t DRIFT = 15
+static const unsigned int TARGET_SPACING = 64;
+static const int64_t DRIFT = 10 * 60;
 inline int64_t FutureDrift(int64_t nTime) { return nTime + DRIFT; }
 
 extern CScript COINBASE_FLAGS;
