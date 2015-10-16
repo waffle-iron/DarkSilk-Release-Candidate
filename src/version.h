d4f16563b6d8b29aed78e5b1cdd3a761bc2478ae
SCDeveloper <silkcoindeveloper@gmail.com>
2015-10-16 20:19:58 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -29,40 +29,40 @@ extern const std::string I2P_NATIVE_DATE;
 //
 // database format versioning
 //
-static const int DATABASE_VERSION = 70509;
+static const int DATABASE_VERSION = 60509;
 
 //
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70020;
+static const int PROTOCOL_VERSION = 60020;
 
 // intial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 70020;
+static const int INIT_PROTO_VERSION = 60020;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70020;
+static const int MIN_PEER_PROTO_VERSION = 60020;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 70020;
+static const int MIN_INSTANTX_PROTO_VERSION = 60020;
 
 //disconnect from Stormnodes older than this proto version
-static const int MIN_SN_PROTO_VERSION = 70020;
+static const int MIN_SN_PROTO_VERSION = 60020;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 70002;
-static const int NOBLKS_VERSION_END = 70006;
+static const int NOBLKS_VERSION_START = 60002;
+static const int NOBLKS_VERSION_END = 60006;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 70020;
+static const int BIP0031_VERSION = 60020;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
-static const int MEMPOOL_GD_VERSION = 70002;
+static const int MEMPOOL_GD_VERSION = 60002;
 
 // reject blocks with non-canonical signatures starting from this version
-static const int CANONICAL_BLOCK_SIG_VERSION = 70016;
+static const int CANONICAL_BLOCK_SIG_VERSION = 60016;
 
 #endif
