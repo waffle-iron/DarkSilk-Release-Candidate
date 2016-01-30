39c8a94ef0e9c2164b147606fea3193fcb05dd87
dev <drkslkdeveloper@gmail.com>
2016-01-30 01:48:47 +0000

    Sanitize

--- src/util.cpp
+++ src/util.cpp
@@ -407,7 +407,7 @@ bool ParseMoney(const char* pszIn, CAmount& nRet)
 
 // safeChars chosen to allow simple messages/URLs/email addresses, but avoid anything
 // even possibly remotely dangerous like & or >
-static string safeChars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890 .,;_/:?@");
+static string safeChars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890 .,;-/:?@");
 string SanitizeString(const string& str)
 {
     string strResult;
