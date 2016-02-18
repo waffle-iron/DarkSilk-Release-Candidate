ccf37f839ad84127355100c629cae744efb4d03a
Silk Network <s.lievens@silknetwork.org>
2016-02-18 17:52:51 +0000

    Includes Pt3 + RPC changes + More

--- src/txdb-leveldb.cpp
+++ src/txdb-leveldb.cpp
@@ -4,7 +4,6 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
-#include "netbase.h"
 #include "net.h"
 #include "protocol.h"
 #include "chainparams.h"
