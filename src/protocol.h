a2e477f7b55388cd7a6abbbe472e0083a443e909
SCDeveloper <spencerlievens@gmail.com>
2016-02-18 11:41:58 +0000

    Changes to Protocol

--- src/protocol.h
+++ src/protocol.h
@@ -49,8 +49,8 @@ class CMessageHeader
     public:
         enum {
             COMMAND_SIZE=12,
-            MESSAGE_SIZE_SIZE=sizeof(int),
-            CHECKSUM_SIZE=sizeof(int),
+            MESSAGE_SIZE_SIZE = sizeof(int),
+            CHECKSUM_SIZE = sizeof(int),
 
             MESSAGE_SIZE_OFFSET=MESSAGE_START_SIZE+COMMAND_SIZE,
             CHECKSUM_OFFSET=MESSAGE_SIZE_OFFSET+MESSAGE_SIZE_SIZE,
@@ -82,20 +82,19 @@ class CAddress : public CService
 
         ADD_SERIALIZE_METHODS;
 
-        template <typename Stream, typename Operation>
-        inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-            CAddress* pthis = const_cast<CAddress*>(this);
-            CService* pip = (CService*)pthis;
-            if (ser_action.ForRead())
-                pthis->Init();
-            if (nType & SER_DISK)
-                READWRITE(nVersion);
-            if ((nType & SER_DISK) ||
-                (nVersion >= CADDR_TIME_VERSION && !(nType & SER_GETHASH)))
-                READWRITE(nTime);
-            READWRITE(nServices);
-            READWRITE(*pip);
-        }
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
+    {
+        if (ser_action.ForRead())
+            Init();
+        if (nType & SER_DISK)
+            READWRITE(nVersion);
+        if ((nType & SER_DISK) ||
+            (nVersion >= CADDR_TIME_VERSION && !(nType & SER_GETHASH)))
+            READWRITE(nTime);
+        READWRITE(nServices);
+        READWRITE(*(CService*)this);
+    }
 
         void print() const;
 
