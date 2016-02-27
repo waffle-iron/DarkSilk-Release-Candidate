a47210829615510885588cab9e5f439afe7e1e3d
Amir Abrams <cryptovote@yandex.com>
2016-02-27 12:35:18 -0600

    BITCOIN -> DARKSILK in addrman

--- src/addrman.h
+++ src/addrman.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_ADDRMAN_H
-#define BITCOIN_ADDRMAN_H
+#ifndef DARKSILK_ADDRMAN_H
+#define DARKSILK_ADDRMAN_H
 
 #include <map>
 #include <set>
@@ -570,4 +570,4 @@ public:
     }
 };
 
-#endif // BITCOIN_ADDRMAN_H
+#endif // DARKSILK_ADDRMAN_H
