43ba9b1b43a9775ca5252b0504aa91cb8f08e3d1
CryptoVote <cryptovote@yandex.com>
2016-03-10 16:07:02 -0600

    Bug fix: removed unneeded code.

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -517,9 +517,6 @@ Value sendtoaddress(const Array& params, bool fHelp)
     if (pwalletMain->IsLocked())
         throw JSONRPCError(RPC_WALLET_UNLOCK_NEEDED, "Error: Please enter the wallet passphrase with walletpassphrase first.");
 
-    if (strError != "")
-        throw JSONRPCError(RPC_WALLET_ERROR, strError);
-
     EnsureWalletIsUnlocked();
 
     SendMoney(address.Get(), nAmount, wtx, fUseIX, fUseSS);
