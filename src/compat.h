b4deaaa2fffc956fe5300f2a34515fa7af075188
SCDeveloper <silkcoindeveloper@gmail.com>
2016-03-27 17:05:31 +0100

    compat.h changes for closesocket

--- src/compat.h
+++ src/compat.h
@@ -23,7 +23,6 @@
 #define FD_SETSIZE 1024 // max number of fds in fd_set
 
 #include <winsock2.h>     // Must be included before mswsock.h and windows.h
-
 #include <mswsock.h>
 #include <windows.h>
 #include <ws2tcpip.h>
@@ -94,4 +93,18 @@ bool static inline IsSelectableSocket(SOCKET s) {
 #endif
 }
 
+inline int myclosesocket(SOCKET& hSocket)
+{
+ if (hSocket == INVALID_SOCKET)
+     return WSAENOTSOCK;
+#ifdef WIN32
+ int ret = closesocket(hSocket);
+#else
+ int ret = close(hSocket);
+#endif
+ hSocket = INVALID_SOCKET;
+ return ret;
+}
+#define closesocket(s)      myclosesocket(s)
+ 
 #endif
